c7b70ca556107328949ac85488f83edcb5975f21
Your Name <pancinha@gmail.com>
2025-09-16 11:16:57 -0300

    pancinha

--- server.js
+++ server.js
@@ -1,44 +1,64 @@
-const express = require("express")
-const sqlite3 = require("sqlite3").verbose()
-const cors = require("cors")
-const bcrypt = require("bcrypt")
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 
-// configurar o servidor
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-//criar banco sqlite3
-const db = new sqlite3.Database("./database.db")
+const db = new sqlite3.Database("./database.db");
 
-//criar tabela usuario
-db.run(`CREATE TABLE IF NOT EXISTS usuarios ( id INTEGER PRIMAY KEY AUTOINCREMENT,
+db.run(`CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT,
     email TEXT,
     senha TEXT
-    )`
-    )
-    //cadaastrar usuario
-    app.post("/usuario", async (req, res) => {
-        console.log(req.body);
-    
-    
-        let nome = req.body.nome
-        let email = req.body.emal
-        let senha = req.body.senha
-    
-        let senhaHash = await bcrypt.hash(senha, 10)
-        console.log(senhaHash);
-    
-        db.run(`INSERT INTO usuarios (nome, email, senha)
-            VALUES (?, ?, ?)`,
-            [nome, email, senhaHash],
+)`);
+
+app.post("/usuarios", async (req, res) => {
+    let nome = req.body.nome;
+    let email = req.body.email;
+    let senha = req.body.senha;
+
+    let senhaHash = await bcrypt.hash(senha, 10);
+
+    db.run(
+        `INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)`,
+        [nome, email, senhaHash],
+        function () {
             res.json({
                 id: this.lastID,
                 nome,
-                email
-            })
-        )
+                email,
+            });
+        }
+    );
+});
+
+app.get("/usuarios", (req, res) => {
+    db.all(`SELECT id, nome, email FROM usuarios`, [], (err, rows) => {
+        res.json(rows);
+    });
+});
+
+
+app.get("/usuarios/:id",(req,res) => {
+    let idUsuario = req.params.id;
+
+    db.get(`SELECT id, nome, email FROM usuarios
+    Where id =?`,
+
+    [idUsuario],(err,result) => {
+        if(result){
+            res.json(result)
+        } else{
+            res.status(404).json({
+                "message" : "Usuário não encontado"
+        })
+        }
     })
+})
+app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
